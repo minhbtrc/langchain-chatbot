48551dbad81486da4f2f5b33b9b651415b05bf4d
Công Minh <minh.btrc@gmail.com>
2023-10-24 15:28:54 +0700

    Add agent as bot brain

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -1,16 +1,23 @@
 import asyncio
-import random
 from queue import Queue
-from typing import Optional, Dict
+from typing import Optional, Dict, Union, List
+from operator import itemgetter
 
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain.callbacks.tracers.langchain import wait_for_all_tracers
 from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableMap
 
-from chatbot.memory import MemoryTypes
+from chatbot.memory import MemoryTypes, MEM_TO_CLASS
 from chatbot.models import ModelTypes
 from chatbot.common.config import Config, BaseObject
 from chatbot.chain import ChainManager
 from chatbot.prompt import BOT_PERSONALITY
-from chatbot.common.objects import Message
+from chatbot.common.objects import Message, MessageTurn
+from chatbot.utils import BotAnonymizer
+from chatbot.tools import CustomSearchTool
 
 
 class Bot(BaseObject):
@@ -20,10 +27,10 @@ class Bot(BaseObject):
             prompt_template: PromptTemplate = None,
             memory: Optional[MemoryTypes] = None,
             model: Optional[ModelTypes] = None,
-            chain_kwargs: Optional[dict] = None,
             memory_kwargs: Optional[dict] = None,
             model_kwargs: Optional[dict] = None,
-            bot_personality: str = BOT_PERSONALITY
+            bot_personality: str = BOT_PERSONALITY,
+            tools: List[str] = None
     ):
         """
         Conversation chatbot
@@ -35,8 +42,6 @@ class Bot(BaseObject):
         :type memory: MemoryTypes
         :param model: Model type
         :type model: ModelTypes
-        :param chain_kwargs: Keyword arguments for LangChain's chain. Default with verbose
-        :type chain_kwargs: Dict
         :param memory_kwargs: Keyword arguments for Memory. Default with ConversationWindowBuffer arguments
         :type memory_kwargs: Dict
         :param model_kwargs: Keyword arguments for Model. Default with VertexAI arguments
@@ -44,17 +49,73 @@ class Bot(BaseObject):
         """
         super().__init__()
         self.config = config if config is not None else Config()
+        self.tools = tools or [CustomSearchTool()]
+        partial_variables = {
+            "bot_personality": bot_personality or BOT_PERSONALITY,
+            "user_personality": "",
+            "tools": "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools]),
+            "tool_names": ", ".join([tool.name for tool in self.tools])
+        }
         self.chain = ChainManager(
             config=self.config,
-            prompt_template=prompt_template,
-            bot_personality=bot_personality,
-            memory=memory,
+            prompt_template="minhi/personality-chat-react-prompt",
             model=model,
-            chain_kwargs=chain_kwargs if chain_kwargs else {"verbose": True},
-            memory_kwargs=memory_kwargs if memory_kwargs else self.default_memory_kwargs,
-            model_kwargs=model_kwargs if model_kwargs else self.default_model_kwargs
+            model_kwargs=model_kwargs if model_kwargs else self.default_model_kwargs,
+            partial_variables=partial_variables
         )
         self.input_queue = Queue(maxsize=6)
+        self._memory = self.get_memory(memory_type=memory, parameters=memory_kwargs)
+        self.anonymizer = BotAnonymizer(config=self.config)
+        self.brain = None
+        self.start()
+
+    @property
+    def memory(self):
+        return self._memory
+
+    def start(self):
+        history_loader = RunnableMap({
+            "input": itemgetter("input"),
+            "agent_scratchpad": itemgetter("intermediate_steps") | RunnableLambda(format_log_to_str),
+            "history": itemgetter("conversation_id") | RunnableLambda(self.memory.load_history)
+        }).with_config(run_name="LoadHistory")
+
+        if self.config.enable_anonymizer:
+            anonymizer_runnable = self.anonymizer.get_runnable_anonymizer().with_config(run_name="AnonymizeSentence")
+            de_anonymizer = RunnableLambda(self.anonymizer.anonymizer.deanonymize).with_config(
+                run_name="DeAnonymizeResponse")
+
+            agent = (history_loader
+                     | anonymizer_runnable
+                     | self.chain.chain
+                     | de_anonymizer
+                     | ReActSingleInputOutputParser())
+
+        else:
+            agent = history_loader | self.chain.chain | ReActSingleInputOutputParser()
+        self.brain = AgentExecutor(agent=agent, tools=self.tools, verbose=True, max_iterations=2)
+
+    def get_memory(
+            self,
+            parameters: dict = None,
+            memory_type: Optional[MemoryTypes] = None
+    ):
+        parameters = parameters or {}
+        if memory_type is None:
+            memory_type = MemoryTypes.BASE_MEMORY
+        if memory_type is not None:
+            if memory_type not in MEM_TO_CLASS:
+                raise ValueError(
+                    f"Got unknown memory type: {memory_type}. "
+                    f"Valid types are: {MEM_TO_CLASS.keys()}."
+                )
+            memory_class = MEM_TO_CLASS[memory_type]
+        else:
+            raise ValueError(
+                "Somehow both `memory` is None, "
+                "this should never happen."
+            )
+        return memory_class(config=self.config, **parameters)
 
     @property
     def default_model_kwargs(self):
@@ -63,41 +124,51 @@ class Bot(BaseObject):
             "temperature": 0.2,
             "top_p": 0.8,
             "top_k": 40,
-            "streaming": True
+            # "streaming": True,
+            "stop": ["\nObservation"]
         }
-
-    @property
-    def default_memory_kwargs(self):
-        return {
-            "k": 5
-        }
-
-    def set_personality(
-            self,
-            bot_personalities: str = BOT_PERSONALITY,
-            user_personality: str = ""
-    ):
-        self.chain.set_personality_prompt(
-            personality_prompt=bot_personalities,
-            user_personality=user_personality
-        )
 
     def reset_history(self, conversation_id: str = None):
-        self.chain.reset_history(conversation_id=conversation_id)
+        self.memory.clear(conversation_id=conversation_id)
+
+    def add_message_to_memory(
+            self,
+            human_message: Union[Message, str],
+            ai_message: Union[Message, str],
+            conversation_id: str
+    ):
+        if isinstance(human_message, str):
+            human_message = Message(message=human_message, role=self.config.human_prefix)
+        if isinstance(ai_message, str):
+            ai_message = Message(message=ai_message, role=self.config.ai_prefix)
+
+        turn = MessageTurn(
+            human_message=human_message,
+            ai_message=ai_message,
+            conversation_id=conversation_id
+        )
+        self.memory.add_message(turn)
+
+    async def __call__(self, message: Message, conversation_id: str):
+        try:
+            try:
+                output = self.brain.invoke({"input": message.message, "conversation_id": conversation_id})['output']
+            except ValueError as e:
+                import regex as re
+                response = str(e)
+                response = re.findall(r".*?Could not parse LLM output: `(.*)`", response)
+                if not response:
+                    raise e
+                output = response[0]
+
+            output = Message(message=output, role=self.config.ai_prefix)
+            return output
+        finally:
+            # Wait for invoke chain finish before push to Langsmith
+            wait_for_all_tracers()
 
     def predict(self, sentence: str, conversation_id: str = None):
         message = Message(message=sentence, role=self.config.human_prefix)
-        return asyncio.run(self.chain(message, conversation_id=conversation_id))
-
-    def send(self):
-        conversation_id = str(random.randint(10000, 99999))
-        while True:
-            sentence = input("User:")
-            output = self.predict(sentence, conversation_id=conversation_id)
-            print("BOT:", output[0].message)
-
-
-if __name__ == "__main__":
-    config = Config()
-    bot = Bot(config=config)
-    bot.send()
+        output = asyncio.run(self(message, conversation_id=conversation_id))
+        self.add_message_to_memory(human_message=message, ai_message=output, conversation_id=conversation_id)
+        return output
--- chatbot/chain.py
+++ chatbot/chain.py
@@ -1,16 +1,11 @@
-from typing import Optional, Union
-from operator import itemgetter
+from typing import Optional
 from langchain.prompts import PromptTemplate
 from langchain import hub
 from langchain.callbacks.tracers.langchain import wait_for_all_tracers
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import RunnableLambda, RunnableMap
 
 from chatbot.common.config import BaseObject, Config
-from chatbot.prompt import *
-from chatbot.common.objects import Message, MessageTurn
-from chatbot.utils import ChatbotCache, BotAnonymizer
-from chatbot.memory import MemoryTypes, MEM_TO_CLASS
+from chatbot.common.objects import Message
+from chatbot.utils import ChatbotCache
 from chatbot.models import ModelTypes, MODEL_TO_CLASS
 
 
@@ -18,52 +13,17 @@ class ChainManager(BaseObject):
     def __init__(
             self,
             config: Config = None,
-            memory: Optional[MemoryTypes] = None,
             model: Optional[ModelTypes] = None,
-            prompt_template: PromptTemplate = None,
-            chain_kwargs: Optional[dict] = None,
-            memory_kwargs: Optional[dict] = None,
+            prompt_template: str = None,
             model_kwargs: Optional[dict] = None,
-            bot_personality: Optional[str] = BOT_PERSONALITY
+            partial_variables: dict = None
     ):
         super().__init__()
         self.config = config if config is not None else Config()
-
-        self._memory = self.get_memory(memory_type=memory, parameters=memory_kwargs)
         self._base_model = self.get_model(model_type=model, parameters=model_kwargs)
-        if prompt_template:
-            self._prompt = prompt_template
-        else:
-            partial_variables = {
-                "bot_personality": bot_personality or BOT_PERSONALITY,
-                "user_personality": "",
-            }
-            self._init_prompt_template(partial_variables=partial_variables)
-        chain_kwargs = chain_kwargs or {}
+        self._init_prompt_template(template_path=prompt_template, partial_variables=partial_variables)
         self._cache = ChatbotCache.create(config=self.config)
-        self.anonymizer = BotAnonymizer(config=self.config)
-        self._init_chain(**chain_kwargs)
-
-    def get_memory(
-            self,
-            memory_type: Optional[MemoryTypes] = None,
-            parameters: Optional[dict] = None
-    ):
-        if memory_type is None:
-            memory_type = MemoryTypes.BASE_MEMORY
-        if memory_type is not None:
-            if memory_type not in MEM_TO_CLASS:
-                raise ValueError(
-                    f"Got unknown memory type: {memory_type}. "
-                    f"Valid types are: {MEM_TO_CLASS.keys()}."
-                )
-            memory_class = MEM_TO_CLASS[memory_type]
-        else:
-            raise ValueError(
-                "Somehow both `memory` is None, "
-                "this should never happen."
-            )
-        return memory_class(config=self.config, **parameters)
+        self._init_chain(model_kwargs=model_kwargs)
 
     def get_model(
             self,
@@ -93,48 +53,13 @@ class ChainManager(BaseObject):
             return model_class(model_name=model_name, **parameters)
         return model_class(**parameters, return_messages=True)
 
-    @property
-    def memory(self):
-        return self._memory
+    def _init_chain(self, model_kwargs:dict):
+        self.chain = (self._prompt | self._base_model.bind(**model_kwargs)).with_config(run_name="GenerateResponse")
 
-    def reset_history(self, conversation_id: str = None):
-        self.memory.clear(conversation_id=conversation_id)
-
-    def _init_chain(self, **kwargs):
-        history_loader = RunnableMap({
-            "input": itemgetter("input"),
-            "history": itemgetter("conversation_id") | RunnableLambda(self.memory.load_history)
-        }).with_config(run_name="LoadHistory")
-
-        response_generator = (self._prompt | self._base_model | StrOutputParser()).with_config(
-            run_name="GenerateResponse")
-
-        if self.config.enable_anonymizer:
-            anonymizer_runnable = self.anonymizer.get_runnable_anonymizer().with_config(run_name="AnonymizeSentence")
-            de_anonymizer = RunnableLambda(self.anonymizer.anonymizer.deanonymize).with_config(
-                run_name="DeAnonymizeResponse")
-
-            self.chain = (history_loader
-                        | anonymizer_runnable
-                        | response_generator
-                        | de_anonymizer)
-        else:
-            self.chain = history_loader | response_generator
-
-        if not isinstance(self._memory, MEM_TO_CLASS[MemoryTypes.CUSTOM_MEMORY]):
-            self.chain = self.chain | self.memory.memory
-
-    def _init_prompt_template(self, partial_variables=None):
-        _prompt: PromptTemplate = hub.pull("minhi/personality-chatbot-prompt")
-        self._prompt = _prompt.partial(**partial_variables)
-
-    def set_personality_prompt(self, bot_personality: str = BOT_PERSONALITY, user_personality: str = ""):
-        partial_variables = {
-            "bot_personality": bot_personality,
-            "user_personality": user_personality
-        }
-        self._init_prompt_template(partial_variables)
-        self._init_chain()
+    def _init_prompt_template(self, template_path: str = None, partial_variables=None):
+        template_path = template_path or "minhi/personality-chatbot-prompt"
+        prompt: PromptTemplate = hub.pull(template_path)
+        self._prompt = prompt.partial(**partial_variables)
 
     async def _predict(self, message: Message, conversation_id: str):
         try:
@@ -151,25 +76,6 @@ class ChainManager(BaseObject):
             include_names=["StreamResponse"]
         )
 
-    def add_message_to_memory(
-            self,
-            human_message: Union[Message, str],
-            ai_message: Union[Message, str],
-            conversation_id: str
-    ):
-        if isinstance(human_message, str):
-            human_message = Message(message=human_message, role=self.config.human_prefix)
-        if isinstance(ai_message, str):
-            ai_message = Message(message=ai_message, role=self.config.ai_prefix)
-
-        turn = MessageTurn(
-            human_message=human_message,
-            ai_message=ai_message,
-            conversation_id=conversation_id
-        )
-        self.memory.add_message(turn)
-
     async def __call__(self, message: Message, conversation_id: str):
         output: Message = await self._predict(message=message, conversation_id=conversation_id)
-        self.add_message_to_memory(human_message=message, ai_message=output, conversation_id=conversation_id)
         return output
